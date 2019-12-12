8f97403d82a786d8d720376ba4321b0f0fa0a181
Wu <Hao.Wu@ricardo.com>
2019-12-12 15:46:23 +0000

    add debtor table and accepting advanced payments

--- ducks_app/server.R
+++ ducks_app/server.R
@@ -4,61 +4,160 @@ library(lubridate)
 library(plotly)
 # library(markdown)
 
-which_week <- function(the_date) {
-  
-  weeks_from_start <- floor((the_date - as_date("2019-11-11"))/7) + 1
-  
-  if (weeks_from_start > 4) {
-    
-    no_week <- weeks_from_start %% 4
-    
-  } else no_week <- weeks_from_start
-  
-  as.numeric(no_week)
-  
-}
-
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
+  current_cycle <- which_week(Sys.Date(), cycle = TRUE)
+
+# calculate pot -----------------------------------------------------------
+
   # read payment sheet
-  payment <- read.csv(url("https://docs.google.com/spreadsheets/d/1_KxXTywIfc3uySg9eIGZ97YWTNUqtiam2HsrND7W9fU/export?usp=sharing&format=csv")) %>%
-    mutate(Date = as_date(Date))
+  payment <- reactiveFileReader(1000, session,
+                                payment_sheet, 
+                                readr::read_csv)  
   
-  # get bills
-  bills <- payment %>%
-    filter(Name == "UTC")
-  periods <- unique(c(as_date("2019-01-01"), bills$Date, Sys.Date()))
-  ## cut bills with periods
-  bills <- bills %>%
-    mutate(period = cut(Date, periods, right = TRUE, labels = (1:(length(periods) - 1))))
+  current_pot <- reactive({
+    
+    
+    # get bills
+    bills <- payment() %>%
+      filter(Name == "UTC")
+    periods <- unique(c(as_date("2019-01-01"), 
+                        bills$Date, 
+                        Sys.Date(), 
+                        as_date("2100-01-01")))
+    ## cut bills with periods
+    bills <- bills %>%
+      mutate(period = cut(Date, periods, right = TRUE, labels = (1:(length(periods) - 1))))
+    
+    # calc pot
+    pot <- payment() %>%
+      filter(Type %in% c("paym")) %>%
+      mutate(period = cut(Date, periods, right = TRUE, labels = 1:(length(periods) - 1)),
+             Name = as.character(Name)) %>%
+      filter(which_week(Date, cycle = TRUE) <= current_cycle) %>% # filter up to current cycle
+      group_by(period, Name) %>% 
+      summarise(pot = sum(Payment)) %>% # summarise the pot for each period
+      group_nest() %>% 
+      left_join(bills[c("period", "Payment")], by = "period") %>%
+      mutate(Payment = tidyr::replace_na(Payment, 0))
+    
+    current_pot <- purrr::reduce2(pot$data, pot$Payment, calc_pot, .init = old_pot)
+    
+    # browser()
+    current_pot
+    
+  })
+
+# calculate debtors -------------------------------------------------------
   
-  # calc pot
-  pot <- payment %>%
-    filter(Type == "paym") %>%
-    mutate(period = cut(Date, periods, right = TRUE, labels = 1:(length(periods) - 1)),
-           Name = as.character(Name)) %>%
-    group_by(period, Name) %>%
-    summarise(pot = sum(Payment)) %>%
-    group_nest() %>%
-    left_join(bills[c("period", "Payment")], by = "period") %>%
-    mutate(Payment = tidyr::replace_na(Payment, 0)) 
-  
-  current_pot <- purrr::reduce2(pot$data, pot$Payment, calc_pot, .init = old_pot)
-  
+  # read play log
+  players <- reactiveFileReader(1000, session,
+                                player_sheet,
+                                readr::read_csv)
+  # calculate debt
+  debt <- reactive({
+    
+    players <- players() %>%
+      mutate(cycle = purrr::map_dbl(Date, which_week, cycle = TRUE)) %>%
+      filter(cycle == current_cycle,
+             is_payp == 0 | is.na(is_payp)) %>% # filter current cycle and not payp plays
+      group_by(cycle) %>%
+      count(Name, name = "times") %>%
+      ungroup()
+    
+    if (nrow(players) != 0) {
+      
+      # players with £10 in the pot or all equal when payments had been taken
+      if (length(unique(current_pot()$pot)) == 1) {
+        
+        players_pot <- current_pot()$Name
+        
+      } else {
+        
+        players_pot <- current_pot() %>%
+          filter(pot == 10) %>%
+          pull(Name)
+        
+      }
+      
+      # players owing money
+      players_owe <- players %>%
+        filter(Name %in% setdiff(players$Name, players_pot))
+      
+      # # calc debt table
+      debt <- players_owe %>%
+        left_join(current_pot(), by = "Name") %>%
+        mutate(pot = tidyr::replace_na(pot, 0),
+               # PAYP = times * 2.5,
+               # debt = 10 - pot
+               debt = times * 2.5)
+      
+    } else {
+      
+      debt <- data.frame()
+      
+    }
+    
+    # browser()
+
+    # get PAYP people
+    payp_players <- payment() %>%
+      filter(Type == "payp") %>%
+      group_by(Name) %>%
+      summarise(paid = sum(Payment))
+    
+    # PAYP plays
+    payp_plays <- players() %>%
+      filter(is_payp == 1) %>%
+      count(Name, name = "times") %>%
+      mutate(owe = times * 2.5)
+    
+    # payp table
+    payp_tbl <- payp_plays%>%
+      left_join(payp_players, by = "Name") %>%
+      mutate(paid = tidyr::replace_na(paid, 0),
+             debt = owe - paid) %>%
+      filter(debt != 0)
+
+    bind_rows(debt, payp_tbl)
+    
+    
+  })
+ 
+
+# plotting ----------------------------------------------------------------
+
   output$no_week <- renderText(
     paste0("WEEK ", which_week(Sys.Date()), " OF 4")
   )
 
   output$pot <- renderPlotly({
     
-    plot_ly(current_pot) %>%
+    plot_ly(current_pot()) %>%
       add_pie(values = ~pot, labels = ~Name, textinfo = "text",
               text = ~paste0(Name, ": ", "£", pot),
               hoverinfo = "text",
               hole = 0.3,
               title = ~paste0("£", sum(pot))) %>%
       config(displayModeBar = FALSE)
+    
+  })
+  
+  output$debtit <- renderTable({
+    
+    debt() %>%
+      filter(debt > 0) %>%
+      select(Name, Debit = debt)
+    
+  })
+  
+  output$credit <- renderTable({
+    
+    debt() %>%
+      filter(debt < 0) %>%
+      mutate(debt = debt * (-1)) %>%
+      select(Name, Credit = debt)
     
   })
 
